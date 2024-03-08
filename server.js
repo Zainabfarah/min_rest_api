be7986e28c3ba3a0877293b610029941916d460c
Zainabfarah <zainabfarah890@gmail.com>
2024-03-08 22:29:11 +0100

    GET

--- server.js
+++ server.js
@@ -6,19 +6,14 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //middleware för att tolka JSON-body i requests
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dokumentation.html')); // Sänder dokumentation.html-filen som respons
-});
 
 //lyssna på port 3000
 app.listen(3000);
 console.log("servern körs på port 3000");
 
-//importera MYSQL-modulen för hantering av databasanslutning
-const mysql = require("mysql");
 //skapar en anslutning till MYSQL-databasen 
 const db = mysql.createConnection({
     host: "localhost",
@@ -31,13 +26,45 @@ const db = mysql.createConnection({
 
   db.connect((err) => {
     if (err) {
+    // skickar ett felmeddelande istället för att krascha servern
       console.error('Ett fel inträffade vid anslutning till databasen: ' + err.stack);
+      //Avslutat processen om man inte kan ansluta till databasen
+      process.exit(1); 
       return;
     }
     console.log('Ansluten till databasen med ID ' + db.threadId);
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dokumentation.html')); // skickar dokumentation-hrml som respons 
+});
+
+//GET /users
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+      if (err) {
+        // Skickar ett serverfel 
+        res.status(500).send('Ett serverfel inträffade vid hämtning av användare');
+      } else {
+        // Skickar resultaten som JSON
+        res.json(results);
+      }
+    });
+  });
+
+  //GET/users/:id 
+  app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query('SELECT * FROM users WHERE id = ?', [userId], (err, results) => {
+      if (err) {
+        res.status(500).send('Ett serverfel inträffade');
+      } else if (results.length > 0) {
+        res.json(results[0]);
+      } else {
+        res.status(404).send('Användaren hittades inte.');
+      }
+    });
+  });
 
 
-  // Hantera en GET-förfrågan till rotadressen genom att skicka en HTML-fil
 
