84dd440d91de031d4eade58bb33e2b100645b7d4
Zainabfarah <zainabfarah890@gmail.com>
2024-03-08 21:56:45 +0100

    databas anslutning

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+
+//lyssna på port 3000
+app.listen(3000);
+console.log("servern körs på port 3000");
+
+//importera MYSQL-modulen för hantering av databasanslutning
+const mysql = require("mysql");
+//skapar en anslutning till MYSQL-databasen 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Mydatabas'
+  });
+
